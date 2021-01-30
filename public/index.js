b51b4d02ab4385f7dd15aa24090ecc94ced59bb9
William <48343678+WiIIiamTang@users.noreply.github.com>
2021-01-30 17:41:25 -0500

    re-add changes to card addition

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 var styleState = true;
-const recipeList = document.getElementById('list1')
+var i=0;
+var baseCard = document.getElementById('recipeCard');
+var firstEntry = true;
+var userCustomImg = '';
 
 function swapCSS() {
     const current = document.getElementById('customStyle');
@@ -8,27 +11,118 @@ function swapCSS() {
 }
 
 function addRecipe() {
-    const newRecipe = document.createElement('div');
-    newRecipe.className = "recipeCard col-xs-12 col-sm-6 col-md-3 text-center";
-    /*get form submission later*/
-    const content = `
-                <div class="card" style="width: 18rem;">
-                    <img src="imgs/recipe1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn card-button text-white">Go somewhere</a>
-                    </div>
-                </div>
-    `
-    newRecipe.innerHTML = content;
-    recipeList.appendChild(newRecipe);
+    if(firstEntry){
+        firstEntry = false
+        var hidden = document.getElementById("outerRecipeCard");
+        hidden.style.display = "block";
+        document.getElementById("cardName").innerHTML = recipeName;
+    }
+    
+    else{
+        var newCard = baseCard.cloneNode(true);
+        console.log(newCard.childNodes)
+        newCard.id = "recipeCard" + ++i;
+        newCard.childNodes[1].id = "cardPic" + i;
+        baseCard.parentNode.appendChild(newCard);
+        
+    }
+
+    console.log(document.forms['recipe-content']['recipe-name']);
+
+    var recipeName = document.forms['recipe-content']['recipe-name'].value;
+    var ingredients = document.forms["recipe-content"]["ingredients"].value;
+    var steps = document.forms["recipe-content"]["steps"].value;
+    var cookTime = document.forms["recipe-content"]["cook-time"].value;
+    var ovenTemp = document.forms["recipe-content"]["oven-temp"].value;
+
+    console.log(recipeName);
+    document.getElementById("cardName").innerHTML = recipeName;
+    document.getElementById("cardPic"+i).src = userCustomImg;
+    console.log(userCustomImg);
 }
 
 function deleteRecipe(parent) {
     const p = parent.parentElement;
     p.remove();
 }
+
+function getImg(event) {
+    var i = document.getElementById('outputImg');
+    i.src = URL.createObjectURL(event.target.files[0]);
+    userCustomImg = i.src;
+}
+
+async function searchRecipe(searchId, resultId) {
+    const userinput = (document.getElementById(searchId)).value;
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + userinput);
+    var data = await response.json();
+    if (data['meals'] != null) {
+        data = data['meals'][Math.floor(Math.random() * data['meals'].length)]
+
+        var ingredients = Object.values(Object.keys(data)
+            .filter(k => k.includes('Ingredient'))
+            .reduce((o, k) => {
+                if (data[k] != '' && data[k] != null) {
+                    o[k] = data[k]
+                }
+                return o
+            }, {}))
+        var img = data['strMealThumb']
+        var title = data['strMeal']
+        
+        //console.log(ingredients)
+        //console.log(data);
+
+        const result = document.getElementById(resultId);
+        result.innerHTML = `
+        <div class="recipeCard col-2 text-center">
+        <div style="width: 18rem; margin-left: 60px; margin-top: 10px">
+            <img src="${img}" class="card-img-top" alt="...">
+            <div>
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${ingredients}</p>
+            </div>
+        </div>
+        </div>
+        `
+        latestSearch = {
+            'title': title,
+            'img' : img,
+            'ingredients': ingredients,
+            'steps': data['strInstructions']
+        }
+    }
+    else {
+        const result = document.getElementById(resultId);
+        result.innerHTML = 'No results found.'
+        latestSearch = null;
+    }
+}
+
+function addRecipeSearch() {
+    // I should probably combine this logic with the other addRecipeCard method above
+
+    if (latestSearch != null) {
+        console.log(latestSearch);
+        if(firstEntry){
+            firstEntry = false
+            var hidden = document.getElementById("outerRecipeCard");
+            hidden.style.display = "block";
+            document.getElementById("cardName").innerHTML = latestSearch['title'];
+        }
+        
+        else{
+            var newCard = baseCard.cloneNode(true);
+            console.log(newCard.childNodes[1].id)
+            newCard.id = "recipeCard" + ++i;
+            newCard.childNodes[1].id = "cardPic" + i;
+            newCard.childNodes[1].src = latestSearch['img'];
+            baseCard.parentNode.appendChild(newCard);
+            
+        }
+    }
+}
+
 
 
 var mainApp = {};
