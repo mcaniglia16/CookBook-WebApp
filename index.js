4f937f7757a1ca60b9cba73cdcefeb74ab2b51ee
mcaniglia16 <50206147+mcaniglia16@users.noreply.github.com>
2021-01-30 13:28:00 -0500

    add recipe fix

--- index.js
+++ index.js
@@ -8,20 +8,18 @@ function swapCSS() {
 }
 
 function addRecipe() {
-    const newRecipe = document.createElement('li');
+    const newRecipe = document.createElement('div');
+    newRecipe.className = "recipeCard col-xs-12 col-sm-6 col-md-3 text-center";
     /*get form submission later*/
     const content = `
-    <div class="recipeCard col-2 text-center">
-        <button class="btn card-button black delete" onclick="deleteRecipe(this)">Remove</button>
-        <div class="card" style="width: 18rem;">
-            <img src="imgs/recipe1.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn card-button text-white">Go somewhere</a>
-            </div>
-        </div>
-    </div>
+                <div class="card" style="width: 18rem;">
+                    <img src="imgs/recipe1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn card-button text-white">Go somewhere</a>
+                    </div>
+                </div>
     `
     newRecipe.innerHTML = content;
     recipeList.appendChild(newRecipe);
