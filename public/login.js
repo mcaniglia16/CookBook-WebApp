ffd037c50b2e577b9ace40cc754afe589e9fbfdc
vliver1 <vliver1@cs.mcgill.ca>
2021-01-30 21:41:16 -0500

    login fix

--- public/login.js
+++ public/login.js
@@ -1,4 +1,22 @@
-(function() {
+var app_fireBase = {};
+(async function() {
+    // console.log("hello firebase");
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+        apiKey: "AIzaSyDrQo0iBlr_bX42jSzambyy1uPEyzesn-c",
+        authDomain: "cookbook-e138e.firebaseapp.com",
+        databaseURL: "https://cookbook-e138e-default-rtdb.firebaseio.com",
+        projectId: "cookbook-e138e",
+        storageBucket: "cookbook-e138e.appspot.com",
+        messagingSenderId: "834194352942",
+        appId: "1:834194352942:web:ee58e2cd76e4550c8168bf",
+        measurementId: "G-WJKYWZGEZ4"
+    };
+    await firebase.initializeApp(firebaseConfig);
+    app_fireBase = firebase;
+
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
     var uiConfig = {
@@ -25,7 +43,6 @@
 
     };
     ui.start('#firebaseui-auth-container', uiConfig);
-
 })()
 
 var styleState = false;
